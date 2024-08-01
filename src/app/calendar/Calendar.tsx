f5a69a0038fa16d5fd33d8a7a7f7edf8de30eb9b
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-07-31 22:46:12 -0700

    Added className to the Cell Component

--- src/app/calendar/Calendar.tsx
+++ src/app/calendar/Calendar.tsx
@@ -1,11 +1,13 @@
+import Cell from "./Cell";
+
 const Calendar = () => {
     return <div className="w-[400px] border">
         <div className="grid grid-cols-7 items-center justify-center text-center">
-            <div>{"<<"}</div>
-            <div>{"<"}</div>
-            <div className="col-span-3">August 2022</div>
-            <div>{">"}</div>
-            <div>{">>"}</div>
+            <Cell>{"<<"}</Cell>
+            <Cell>{"<"}</Cell>
+            <Cell className="col-span-3">August 2022</Cell>
+            <Cell>{">"}</Cell>
+            <Cell>{">>"}</Cell>
         </div>
     </div>;
 };
--- src/app/calendar/Cell.tsx
+++ src/app/calendar/Cell.tsx
@@ -1,7 +1,9 @@
-interface Props extends React.PropsWithChildren {}
+interface Props extends React.PropsWithChildren {
+    className?: string;
+}
 
-const Cell: React.FC<Props> = ({children}) => {
-    return <div>{children}</div>
+const Cell: React.FC<Props> = ({className, children}) => {
+    return <div className={className}>{children}</div>
 };
 
 export default Cell;
