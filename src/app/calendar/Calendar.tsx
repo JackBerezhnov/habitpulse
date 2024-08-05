ece77069c752e5b15fcca2abb05c2a7bb33c81e9
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-04 23:07:37 -0700

    Added Days

--- src/app/calendar/Calendar.tsx
+++ src/app/calendar/Calendar.tsx
@@ -1,13 +1,20 @@
 import Cell from "./Cell";
 
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 const Calendar = () => {
-    return <div className="w-[400px] border">
+    return <div className="w-[400px] border-t border-l">
         <div className="grid grid-cols-7 items-center justify-center text-center">
             <Cell>{"<<"}</Cell>
             <Cell>{"<"}</Cell>
             <Cell className="col-span-3">August 2022</Cell>
             <Cell>{">"}</Cell>
             <Cell>{">>"}</Cell>
+
+            {daysOfWeek.map((day) => (
+                // eslint-disable-next-line react/jsx-key
+                <Cell className="text-sm font-bold">{day}</Cell>
+            ))}
         </div>
     </div>;
 };
