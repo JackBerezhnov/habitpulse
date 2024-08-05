366068495486bc0055aa594d33b644f8aa2816a3
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-04 22:59:50 -0700

    Added clsx

--- src/app/calendar/Cell.tsx
+++ src/app/calendar/Cell.tsx
@@ -1,9 +1,11 @@
+import clsx from "clsx";
+
 interface Props extends React.PropsWithChildren {
     className?: string;
 }
 
 const Cell: React.FC<Props> = ({className, children}) => {
-    return <div className={className}>{children}</div>
+    return <div className={clsx("h-12 flex items-center justify-center border-b border-r", className)}>{children}</div>
 };
 
 export default Cell;
