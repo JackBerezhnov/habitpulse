cd7b0da0674f5d42714311ff0e92e326c3902d3d
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-14 18:30:01 -0700

    Created handleClickDate function

--- src/app/calendar/Calendar.tsx
+++ src/app/calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import { differenceInDays, endOfMonth, startOfMonth, sub, format, add } from "date-fns";
+import { differenceInDays, endOfMonth, startOfMonth, sub, format, add, setDate } from "date-fns";
 import Cell from "./Cell";
 
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -19,7 +19,12 @@ const Calendar: React.FC<Props> = ({ value = new Date(), onChange }) => {
     const prevMonth = () => onChange && onChange(sub(value, { months: 1 }));
     const nextMonth = () => onChange && onChange(add(value, { months: 1 }));
     const prevYear = () => onChange && onChange(sub(value, { years: 1 }));
-    const nextYear = () => onChange && onChange(add(value, { years: 1 })); 
+    const nextYear = () => onChange && onChange(add(value, { years: 1 }));
+
+    const handleClickDate = (index: number) => {
+        const date = setDate(value, index);
+        onChange && onChange(date);
+    }
 
     return <div className="w-[400px] border-t border-l">
         <div className="grid grid-cols-7 items-center justify-center text-center">
@@ -40,7 +45,7 @@ const Calendar: React.FC<Props> = ({ value = new Date(), onChange }) => {
             {Array.from({length: numDays}).map((_, index) => {
                 const date = index + 1; 
 
-                return <Cell key={date}>{date}</Cell>;
+                return <Cell onClick={() => handleClickDate(index)} key={date}>{date}</Cell>;
             })}
 
             {Array.from({length: suffixDays}).map((_, index) => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { format } from "date-fns";
 import { useState } from "react";
 import Calendar from "./calendar/Calendar";
 
@@ -6,7 +7,11 @@ export default function Home() {
   const [currentDate, setCurrentDate] = useState(new Date("2022-02-01"));
 
   return (
-    <div className="mt-16 flex flex-col items-center">
+    <div className="mt-16 flex flex-col items-center gap-8">
+      <div>
+        <p>Selected Date: {format(currentDate, 'dd LLLL yyyy')}</p>
+      </div>
+
       <Calendar value={currentDate} onChange={setCurrentDate}/>
     </div>
   );
