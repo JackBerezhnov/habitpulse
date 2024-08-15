64b26cd2ebd8fbe7d038296b1061591073dd505e
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-14 18:38:36 -0700

    Added handleSetToday function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,19 @@ import { useState } from "react";
 import Calendar from "./calendar/Calendar";
 
 export default function Home() {
-  const [currentDate, setCurrentDate] = useState(new Date("2022-02-01"));
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const handleSetToday = () => setCurrentDate(new Date());
 
   return (
     <div className="mt-16 flex flex-col items-center gap-8">
-      <div>
+      <div className="flex flex-col items-center gap-2">
         <p>Selected Date: {format(currentDate, 'dd LLLL yyyy')}</p>
 
-        <button className="px-4 py-1 rounded text-white bg-blue-600 hover:bg-blue-700 active:bg-blue-800">Today</button>
+        <button
+          onClick={handleSetToday} 
+          className="text-sm px-4 py-1 rounded text-white bg-blue-600 hover:bg-blue-700 active:bg-blue-800"
+        >Today</button>
       </div>
 
       <Calendar value={currentDate} onChange={setCurrentDate}/>
