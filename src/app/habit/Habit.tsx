25d622d01b8440f8b8ee1f474db58be5c134cc85
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-15 21:09:20 -0700

    Created Habit component

---
+++ src/app/habit/Habit.tsx
@@ -0,0 +1,11 @@
+interface HabitProps {
+    title: string;
+}
+
+const Habit: React.FC<HabitProps> = ({ title }) => {
+    return (
+        <div><h3>{title}</h3></div>
+    );
+};
+
+export default Habit;
