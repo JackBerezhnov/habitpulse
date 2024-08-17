fd19d24571bb41fb170322ef3d85657e5688d67e
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-16 22:14:40 -0700

    Added some style to the habits

--- src/app/habit/Habit.tsx
+++ src/app/habit/Habit.tsx
@@ -4,7 +4,7 @@ export interface HabitProps {
 
 const Habit: React.FC<HabitProps> = ({ name }) => {
     return (
-        <div><h3>{name}</h3></div>
+        <div><button className="border-solid border-2 py-2 w-72 mt-5">{name}</button></div>
     );
 };
 
