8fdcd65366503f1ed4a4adec9051ed94d4e1572c
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-16 22:22:17 -0700

    Added Calendar Component into Habit Component

--- src/app/habit/Habit.tsx
+++ src/app/habit/Habit.tsx
@@ -1,10 +1,19 @@
+import Calendar from "../calendar/Calendar";
+import { useState } from "react";
+
 export interface HabitProps {
     name: string;
 }
 
 const Habit: React.FC<HabitProps> = ({ name }) => {
+    
+    const [currentDate, setCurrentDate] = useState(new Date());
+
     return (
-        <div><button className="border-solid border-2 py-2 w-72 mt-5">{name}</button></div>
+        <div>
+            <button className="border-solid border-2 py-2 w-72 mt-5">{name}</button>
+            <Calendar value={currentDate} onChange={setCurrentDate}/>
+        </div>
     );
 };
 
