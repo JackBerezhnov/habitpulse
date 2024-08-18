7a16abb4c474cc9dff62c32647741c5e4af74e2e
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-17 22:20:49 -0700

    Removed selected date in Habit

--- src/app/habit/Habit.tsx
+++ src/app/habit/Habit.tsx
@@ -13,16 +13,8 @@ const Habit: React.FC<HabitProps> = ({ name }) => {
 
     return (
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn m-1">{name}</div>
+          <div tabIndex={0} role="button" className="btn m-1">{name} <span>Last time:</span>{format(currentDate, 'dd LLLL yyyy')}</div>
           <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-            <button className="border-solid border-2 py-2 w-72 mt-5">{name}</button>
-            <div className="flex flex-col items-center gap-2">
-                <p>Selected Date: {format(currentDate, 'dd LLLL yyyy')}</p>
-                <button
-                onClick={handleSetToday} 
-                className="text-sm px-4 py-1 rounded text-white bg-blue-600 hover:bg-blue-700 active:bg-blue-800"
-                >Today</button>
-            </div>
             <Calendar value={currentDate} onChange={setCurrentDate}/>
           </ul>
         </div>
