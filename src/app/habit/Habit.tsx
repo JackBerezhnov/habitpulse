dca101d12dbca8e0d0b997d625f0f09a5a33bc52
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-17 22:36:19 -0700

    Some changes

--- src/app/habit/Habit.tsx
+++ src/app/habit/Habit.tsx
@@ -14,7 +14,7 @@ const Habit: React.FC<HabitProps> = ({ name }) => {
     return (
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn m-1">{name} <span>Last time:</span>{format(currentDate, 'dd LLLL yyyy')}</div>
-          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-100 p-5 shadow">
             <Calendar value={currentDate} onChange={setCurrentDate}/>
           </ul>
         </div>
