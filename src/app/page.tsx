27e30efeac9bced3f1a48fb9bc9977916b2a0c7f
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-08-18 10:48:09 -0700

    Implemented Logout Feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import { useState } from "react";
 import Habit, {HabitProps}  from "./habit/Habit";
+import { account } from "./appwrite";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
   const [habits, setHabits] = useState<HabitProps[]>([]);
   const [habitName, setHabitName] = useState<string>('');
+  const router = useRouter();
 
   const handleAddHabit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -19,8 +22,19 @@ export default function Home() {
     setHabitName('');
   }
 
+  const logout = async () => {
+    try {
+      await account.deleteSession('current');
+      console.log('Logged out successfully');
+      router.push("/login");
+    } catch (error) {
+      console.log('Error logging out: ', error);
+    }
+  }
+
   return (
     <div className="pt-20 flex flex-col items-center gap-8 hero bg-base-200 min-h-screen">
+      <button className="btn btn-outline" onClick={logout}>Logout</button>
       <form onSubmit={handleAddHabit}>
         <input 
           className="mr-10 input input-bordered w-full max-w-xs"
