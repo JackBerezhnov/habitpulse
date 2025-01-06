23115b15b2a1e6dd13c6a9b428d678e115d80528
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2025-01-05 19:25:56 -0800

    Clean loggin screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,9 +12,6 @@ interface User {
 
 const LoginPage: React.FC = () => {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [name, setName] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
   const router = useRouter();
 
@@ -52,15 +49,6 @@ const LoginPage: React.FC = () => {
     }
   };
 
-  const register = async () => {
-    try {
-      await account.create(ID.unique(), email, password, name);
-      await login(email, password);
-    } catch (error) {
-      console.error("Failed to register:", error);
-    }
-  };
-
   const logout = async () => {
     try {
       await account.deleteSession("current");
@@ -71,8 +59,8 @@ const LoginPage: React.FC = () => {
   };
 
   if (loading) {
-    return <div className="hero bg-base-200 min-h-screen">
-      <p>Loading...</p>
+    return <div className="flex flex-col justify-center items-center gap-8 hero bg-base-200 h-[100vh]">
+      <span className="loading loading-spinner loading-lg"></span>
     </div>;
   }
 
@@ -89,69 +77,8 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="hero bg-base-200 min-h-screen">
-      <p>Not logged in</p>
       <form>
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="h-4 w-4 opacity-70">
-          <path
-            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-          <path
-            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-        </svg>
-        <input
-          className="grow"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="h-4 w-4 opacity-70">
-          <path
-            fillRule="evenodd"
-            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-            clipRule="evenodd" />
-        </svg>
-        <input
-          className="grow"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="h-4 w-4 opacity-70">
-          <path
-            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-        </svg>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-        <button type="button" className="btn btn-active btn-primary" onClick={() => login(email, password)}>
-          Login
-        </button>
-        <button type="button" className="btn btn-active btn-ghost" onClick={register}>
-          Register
-        </button>
-        <button type="button" id="btn-siwg" onClick={loginSIWG}>
+        <button type="button" id="btn-siwg" className="btn btn-primary" onClick={loginSIWG}>
           Sign in with Google
         </button>
       </form>
