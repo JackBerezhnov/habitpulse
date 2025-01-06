d6c6c5905e58b13cdd33a2416f26f6f4cdcc626c
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2025-01-05 20:29:12 -0800

    Change redirection link after login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -34,7 +34,7 @@ const LoginPage: React.FC = () => {
   const loginSIWG = async () => {
     account.createOAuth2Session(
       OAuthProvider.Google,
-      "https://habitpulse-82ub4kc3p-jackberezhnovs-projects.vercel.app/"
+      "/"
     )
   }
 
