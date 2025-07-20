7650f8237e3e05499b732bfa794e5271891852d6
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2025-07-20 11:48:44 -0700

    Updated endpoint for new Appwrite version

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -3,7 +3,7 @@ import { Client, Account, Databases, Avatars } from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
     .setProject(`${process.env.NEXT_PUBLIC_PROJECT_ID}`);
 
 export const account = new Account(client);
