a065f2211e2e124cdf997ed6c160c7dc29ae42b7
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2025-07-27 19:27:28 -0700

    Changed API endpoint for appwrite to the my custom domain

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -3,7 +3,7 @@ import { Client, Account, Databases, Avatars, OAuthProvider } from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setEndpoint('https://auth.habit-pulse.com/v1')
     .setProject(`${process.env.NEXT_PUBLIC_PROJECT_ID}`);
 
 export const account = new Account(client);
